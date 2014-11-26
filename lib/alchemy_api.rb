8b7631d73fbeff4c0fb23a2d0a6d0a1cbbff389a
Kenneth Lim <kennethlimjf@gmail.com>
2014-11-27 02:09:17 +0800

    Implemented AlchemyAPI module to get sentiment analysis. Implemented Sentwix module with analyze movie method

---
+++ lib/alchemy_api.rb
@@ -0,0 +1,50 @@
+require 'rubygems'
+require 'net/http'
+require 'uri'
+require 'json'
+
+module AlchemyAPI
+
+  class DataInvalidError < StandardError; end
+
+  BASE_URL = 'http://access.alchemyapi.com/calls'
+  TEXT_SENTIMENT_ENDPOINT = '/text/TextGetTextSentiment'
+  API_KEY = ENV['ALCHEMY_API_KEY']
+  OUTPUT_MODE = 'json'
+
+  def self.sentiment(data)
+    raise DataInvalidError if data.nil? || data.strip.empty?
+    analyze(data)
+  end
+
+  private
+
+  def self.analyze(data)
+    options = set_options(data) 
+    request, uri = create_request_object(options)
+
+    res = Net::HTTP.start(uri.host, uri.port) do |http|
+      http.request(request)
+    end
+
+    JSON.parse(res.body)
+  end
+
+  def self.set_options(data)
+    options = {
+      'text' => data,
+      'apikey' => API_KEY,
+      'outputMode' => OUTPUT_MODE
+    }
+  end
+
+  def self.create_request_object(options)
+    url = BASE_URL + TEXT_SENTIMENT_ENDPOINT
+    uri = URI.parse(url)
+    request = Net::HTTP::Post.new(uri.request_uri)
+    request.set_form_data(options)
+    request['Accept-Encoding'] = 'identity'
+    [request, uri]
+  end
+
+end
---
+++ lib/sentwix.rb
@@ -0,0 +1,34 @@
+require 'sentwix/twitter_wrapper'
+require 'alchemy_api'
+
+module Sentwix
+
+  class InvalidMovieError < StandardError; end
+
+  DATA_UNAVAILABLE = "Data unavailable"
+  DATA_LIMIT = 50
+
+  def self.analyze_movie(movie_title)
+    raise InvalidMovieError if movie_title.nil? || movie_title.strip.empty?
+
+    twitter = TwitterWrapper.new
+    search_results = twitter.search(movie_title, {count: DATA_LIMIT})
+
+    analysis = []
+    search_results.each_with_index do |tweet, index|
+      break if index == 5
+      sen_result = AlchemyAPI.sentiment(tweet.full_text)
+      sen_type = sen_result['docSentiment'] ?
+        sen_result['docSentiment']['type'] : DATA_UNAVAILABLE
+
+      analysis << {
+        text: tweet.full_text,
+        type: sen_type
+      }
+    end
+
+    analysis.group_by{ |item| item[:type] }
+            .map{ |type, group| [type, group.count] }
+  end
+
+end
