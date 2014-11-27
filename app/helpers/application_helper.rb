171d2b04c65c146dd81d3e91a05f1721074971bb
Kenneth Lim <kennethlimjf@gmail.com>
2014-11-28 01:04:32 +0800

    Implement time period for tweets

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,13 @@
 module ApplicationHelper
   def time_format(datetime)
-    datetime.localtime.strftime("%d %b %Y, %H:%M:%S")
+    if datetime
+      datetime.localtime.strftime("%d %b %Y, %H:%M:%S")
+    end
+  end
+
+  def time_period(datetime)
+    if datetime
+      "#{distance_of_time_in_words_to_now(datetime)} ago"
+    end
   end
 end
