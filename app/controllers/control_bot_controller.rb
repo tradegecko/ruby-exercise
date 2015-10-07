04f14753f39d77a6b52c94e07a19c277502f1535
Felix Sperling <f.sperling@gmx.de>
2015-10-07 10:47:21 +0800

    more cleanup

--- app/controllers/control_bot_controller.rb
+++ app/controllers/control_bot_controller.rb
@@ -1,11 +1,6 @@
 require 'twitter_api_helper'
 
 class ControlBotController < ApplicationController
-
-  def view
-    @result = TwitterApiHelper.getUserName
-  end
-
   def reply
    @mention = TwitterApiHelper.answerToMentions 
   end
