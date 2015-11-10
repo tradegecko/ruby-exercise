b6c425573d0afbccae50c12a708e4d0870da83be
Yuri Kovalov <yuri@yurikoval.com>
2015-11-10 23:19:14 +0900

    Use select themes for dreamscope

--- lib/dream_scope_api.rb
+++ lib/dream_scope_api.rb
@@ -26,88 +26,26 @@ class DreamScopeApi
 
   STYLES = %i(
     angel_hair
-    art_deco
-    autumn
-    blue
-    bonfire
     botanical_dimensions
-    bw_pastel
     cafe_terrace
     calligraphy
-    cemetary
-    charcoal_sketch
-    charcoal
     clashing_by_clayton_kashuba
-    codex_davinci
-    cold_glow
-    color_pastel
-    crayon
-    cubism
-    durer_melencolia
-    energy_buzz
-    english_breakfast
-    engraved_clay
-    facelift
-    fanime
-    fire
-    glitch_life
-    graffiti_2
-    graffiti
-    graveyard_mist
     greek_pottery
     halloween_clinton
-    halloween_frankenstein
     halloween_pirate
-    halloween_skull
-    halloween_spider
-    halloween_trump
-    halloween_web
-    haunted_charcoal
-    impasto
     inceptionist_painting
-    Inkblot
-    iridescent
     jeweled_bird
-    jfk_portrait
     kandinsky
     library_of_alexandria
-    light_painting
-    matisse_boat
-    matisse_cliffs
-    matisse_farms
-    median
-    midnight_smoke
-    mirage
     money
-    motherboard_2
     motherboard
-    nighthawk
-    northern_renaissance
-    oil_on_canvas
-    oscilloscope
-    petrified_wood
+    motherboard_2
     picasso
-    pumpkin
-    salvia
-    sandstorm
-    self_transforming_machine_elves
-    sketchasketch
-    snow_crash
-    springtime
-    stained_glass_2
     stained_glass
+    stained_glass_2
     starry_night_landscape
     starry_night_portrait
     street_zebra
     supertrippy
-    the_scream
-    the_wave
-    trippy
-    tropical_sunrise
-    two_girls
-    van_gogh_portrait
-    watercolor_casset_portrait
-    watercolor_manet_portrait
-    woman_weeping
   ).freeze
 end
