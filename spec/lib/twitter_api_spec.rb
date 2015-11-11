d3333f6304e95ef725429af3845188d298218b81
Yuri Kovalov <yuri@yurikoval.com>
2015-11-12 07:05:09 +0900

    Test query string for twitter

--- spec/lib/twitter_api_spec.rb
+++ spec/lib/twitter_api_spec.rb
@@ -8,4 +8,9 @@ RSpec.describe TwitterApi do
     its(:count) { is_expected.to eql 100 }
     its(:first) { is_expected.to be_a Twitter::Tweet }
   end
+
+  it "#keyword_with_images returns correct string" do
+    query = api.send(:keyword_with_images, 'some keywords')
+    expect(query).to eql 'some keywords -RT filter:images'
+  end
 end
