a4c3869501fe623750be489e69c42ec6f23493f0
Arun Kumar <rako.aka.arun@gmail.com>
2015-11-17 07:51:13 +0800

    Wrapper around twitter api to post a simple tweet with message.

--- lib/twitter_api.rb
+++ lib/twitter_api.rb
@@ -3,6 +3,7 @@ require 'twitter'
 class TwitterApi
 
   DEFAULT_TWEET_COUNT=100
+  MAX_TWEET_MESSAGE_LENGTH=140
 
   def initialize
     @client = Twitter::REST::Client.new do |config|
@@ -17,8 +18,18 @@ class TwitterApi
     @client.search(with_search_suffix(keyword), result_type: 'recent').take(DEFAULT_TWEET_COUNT)
   end
 
+  def tweet(message)
+    validate_message(message)
+    @client.update(message)
+  end
+
   private
   def with_search_suffix(keyword)
     "#{keyword} -RT"
+  end
+
+  def validate_message(message)
+    raise(ArgumentError, "Message can't be empty") if message.blank?
+    raise(ArgumentError, "Message more than #{MAX_TWEET_MESSAGE_LENGTH} characters") if message.length > MAX_TWEET_MESSAGE_LENGTH
   end
 end
--- spec/lib/twitter_api_spec.rb
+++ spec/lib/twitter_api_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe TwitterApi do
+  subject { TwitterApi.new }
 
   describe '#fetch_tweets' do
-    subject { TwitterApi.new }
-
     context 'Some tweets found by the keyword' do
       it 'should return an Array of Twitter::Tweet' do
         VCR.use_cassette('fetch_tweets_with_results') do
@@ -24,6 +23,31 @@ RSpec.describe TwitterApi do
           expect(tweets).to be_a Array
           expect(tweets).to be_empty
         end
+      end
+    end
+  end
+
+  describe '#tweet' do
+    context 'with proper message' do
+      it 'should post the tweet and returns the posted tweet' do
+        VCR.use_cassette('tweet_a_proper_message') do
+          tweet = subject.tweet('Hola! Everyone')
+
+          expect(tweet).to be_a Twitter::Tweet
+        end
+      end
+    end
+
+    context 'with empty message' do
+      it 'should raise an Argument Error' do
+        expect{ subject.tweet('') }.to raise_error(ArgumentError, "Message can't be empty")
+      end
+    end
+
+    context 'with long message' do
+      it 'should raise an Argument Error' do
+        message = 'AReallyLongMessageToBreakTheDefaultMessageLengthOfTwitter' * 3
+        expect{ subject.tweet(message) }.to raise_error(ArgumentError, 'Message more than 140 characters')
       end
     end
   end
