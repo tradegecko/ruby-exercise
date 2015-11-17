4551e3795daa1e8678ef313195d289dab11a5eb3
Arun Kumar <rako.aka.arun@gmail.com>
2015-11-17 08:30:08 +0800

    Giphy wraper to find random gif url based on a keyword

---
+++ spec/lib/giphy_api_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe GiphyApi do
+
+  describe '#fetch_random_gif' do
+    subject { GiphyApi.new.fetch_random_gif(keyword) }
+
+    context 'with no keyword' do
+      let(:keyword) { nil }
+      it 'should return a random gif url' do
+        VCR.use_cassette('giphy/random_gif_without_a_search_keyword') do
+          expect(subject).to be_a URI
+        end
+      end
+    end
+
+    context 'with non empty keyword' do
+      context 'result found on the keyword' do
+        let(:keyword) { 'Hola' }
+        it 'should return a random gif url' do
+          VCR.use_cassette('giphy/random_search_with_a_gif_result') do
+            expect(subject).to be_a URI
+          end
+        end
+      end
+
+      context 'result found on the keyword' do
+        let(:keyword) { 'ThereShouldBeNoWayThatThereIsAGifUnderThisName' }
+        it 'should return nil' do
+          VCR.use_cassette('giphy/random_search_with_empty_result') do
+            expect(subject).to be_nil
+          end
+        end
+      end
+    end
+  end
+end
