11f3aa4f57e5f85bd194e5c10caa713048166a77
Bob Cao <bobintornado@gmail.com>
2015-12-06 07:02:47 +0800

    Setup logging in active job

--- app/jobs/deep_dream_job.rb
+++ app/jobs/deep_dream_job.rb
@@ -4,20 +4,20 @@ class DeepDreamJob < ActiveJob::Base
   def perform
     # perform dreaming job later
     twitter_client = TwitterClient.new
-    Rails.logger.debug 'searching tweets....'
+    Rails.logger.info 'searching tweets....'
     results = twitter_client.search_tweets_with_images_by_keyword(ENV['DREAM_KEYWORD'])
     
-    Rails.logger.debug 'getting image....'
+    Rails.logger.info 'getting image....'
     original_media_url = results.first.media.first.media_url.to_s
     original_image_path = FileHandler.download_file_from_url original_media_url
     
-    Rails.logger.debug 'processing image....'
+    Rails.logger.info 'processing image....'
     dreamscopeapp_client = DreamscopeappClient.new(original_image_path.to_s)
     filter_image_url = dreamscopeapp_client.pull
     filter_image_path = FileHandler.download_file_from_url filter_image_url
     
-    Rails.logger.debug 'tweeting image....'
+    Rails.logger.info 'tweeting image....'
     twitter_client.tweet_dreamed_image("Deep dream", File.new(filter_image_path))
-    Rails.logger.debug 'new tweet completed!'
+    Rails.logger.info 'new tweet completed!'
   end
 end
