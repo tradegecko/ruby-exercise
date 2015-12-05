b208e0129990b5e92d3ca253330179ceeb193107
Bob Cao <bobintornado@gmail.com>
2015-12-05 22:17:59 +0800

    Implemented searching tweets method

--- lib/twitter_client.rb
+++ lib/twitter_client.rb
@@ -6,7 +6,28 @@ class TwitterClient
     @client = init
   end
 
+  def search_tweets_with_images_by_keyword keyword
+    query_str = build_query_str(keyword)
+    @client.search(query_str, {result_type: 'recent', count: 1})
+  end
+
   private
+
+  def build_query_str keyword
+    array = Array.new
+    array << keyword
+    array = add_filter_image array
+    array = add_filter_no_rt array
+    query_str = array.join(' ')
+  end
+
+  def add_filter_image array
+    array << 'filter:images'
+  end
+
+  def add_filter_no_rt array 
+    array << '-RT'
+  end
 
   def init
     Twitter::REST::Client.new do |config|
