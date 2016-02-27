d9fc2c727bb99bbb3e5fcb99de373d1a44ac7e58
An The Dung <anthedung@gmail.com>
2016-02-27 08:41:21 +0800

    Stripping off the fun AngularJS
    - remove AngularJS to continue Dev / refactoring work

--- client/src/app/index.module.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  'use strict';
-
-  angular
-    .module('angularRails', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ngMaterial', 'toastr']);
-
-})();
