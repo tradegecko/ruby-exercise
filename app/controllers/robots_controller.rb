4f3084e0679d4840de4d9d57ca6ab2b28cfcda1c
Wen Yu <wongwy93@gmail.com>
2016-04-08 15:50:39 +0800

    Tweet according to action

--- app/controllers/robots_controller.rb
+++ app/controllers/robots_controller.rb
@@ -1,4 +1,23 @@
 class RobotsController < ApplicationController
   def index
   end
+
+  def send_weather
+  	client = TwitterClient.new
+  	client.post_tweet
+  	redirect_to weather_send_path
+  end
+
+  def random_send_weather
+  	client = TwitterClient.new
+  	client.random_post_tweet
+  	redirect_to random_weather_send_path
+  end
+
+  def weather_send
+  end
+
+  def random_weather_send
+  end
+
 end
---
+++ lib/twitter_client.rb
@@ -0,0 +1,46 @@
+class TwitterClient
+
+	include ApplicationHelper
+	
+	def initialize
+		@client = twitter_credentials
+	end
+
+	def post_tweet
+		result = request_ip
+		coordinate(result)
+		@client.update("Visitor from #{result}: The weather is #{@forecast.summary} and #{@forecast.temperature} celcius")
+	end
+
+	def random_post_tweet
+		coordinate(nil)
+		@client.update("Today's Random City: #{@city}'s weather is #{@forecast.summary} and #{@forecast.temperature} celcius")
+	end
+
+	def coordinate(result)
+		if result.nil?
+			random_lat, random_long = Random.new.rand(-90..90) , Random.new.rand(-180..180)
+			while (location = (Geocoder.search("#{random_lat}, #{random_long}"))) == []
+				location = (Geocoder.search("#{random_lat}, #{random_long}"))
+			end		
+			location[0].city.empty? ? @city = 'Unknown City' : @city = location[0].city
+		else
+			location = Geocoder.search(result)
+			@city = result
+		end
+		lat, long = location[0].latitude, location[0].longitude
+		@forecast = ForecastIO.forecast(lat, long, params: { units: 'si' }).currently
+	end
+
+	private
+
+	def twitter_credentials
+		Twitter::REST::Client.new do |config|
+		  config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
+	    config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
+	    config.access_token = ENV['TWITTER_ACCESS_TOKEN']
+	    config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
+	  end
+	end
+
+end
