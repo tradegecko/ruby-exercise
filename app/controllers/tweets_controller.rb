tradegecko/ruby-exercise

commit fbefaf8250124edeeba06d0d79f9313e2a93ae9f

Add frontend for submitting new tweet

app/controllers/tweets_controller.rb
@@ -1,0 +1,15 @@
+class TweetsController < ApplicationController
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new(params.require(:tweets).permit(:content))
+    if @tweet.save
+      redirect_to root_path, notice: 'Tweet submitted'
+    else
+      flash.now[:warning] = 'Tweet submission failed'
+      render :new
+    end
+  end
+end

spec/controllers/tweets_controller_spec.rb
@@ -1,0 +1,56 @@
+require 'rails_helper'
+
+RSpec.describe TweetsController, type: :controller do
+
+  describe "GET #new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+    
+    context "assigns" do
+      it "is a tweet" do
+        get :new
+        expect(assigns[:tweet]).to be_a Tweet
+      end
+
+      it "is new record" do
+        get :new
+        expect(assigns[:tweet]).to be_new_record
+      end
+    end
+  end
+
+  describe "POST #create" do
+    context "if invalid" do
+      before do
+        post :create, tweets: { content: '' }
+      end
+      it "should render #new" do
+        expect(response).to render_template 'tweets/new'
+      end
+
+      it "should have correct flash" do
+        expect(flash.now[:warning]).to eql 'Tweet submission failed'
+      end
+    end
+
+    context "if valid" do
+      it "should create new tweet" do
+        expect{
+          post :create, tweets: { content: 'New content' }
+        }.to change(Tweet, :count).by 1
+      end
+
+      it "should have correct redirect" do
+        post :create, tweets: { content: 'New content' }
+        expect(response).to redirect_to root_path
+      end
+
+      it "should have correct flash" do
+        post :create, tweets: { content: 'New content' }
+        expect(flash[:notice]).to eql 'Tweet submitted'
+      end
+    end
+  end
+end

.gitignore
@@ -15,3 +15,4 @@
 /log/*.log
 /tmp
 .env
+.powrc

app/views/tweets/new.html.erb
@@ -1,0 +1,4 @@
+<%= form_for(@tweet) do |f| %>
+  <%= f.text_area :content %>
+  <%= f.submit %>
+<% end %>

config/routes.rb
@@ -1,56 +1,5 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
   root 'robots#index'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  resources :tweets
 end

commit 13aba328f1ab9a305c3ad966ae47f75c8e865ff9

Fix strong params bug introduced in fbefaf82

app/controllers/tweets_controller.rb
@@ -4,7 +4,7 @@ class TweetsController < ApplicationController
   end
 
   def create
-    @tweet = Tweet.new(params.require(:tweets).permit(:content))
+    @tweet = Tweet.new(params.require(:tweet).permit(:content))
     if @tweet.save
       redirect_to root_path, notice: 'Tweet submitted'
     else

spec/controllers/tweets_controller_spec.rb
@@ -24,7 +24,7 @@ RSpec.describe TweetsController, type: :controller do
   describe "POST #create" do
     context "if invalid" do
       before do
-        post :create, tweets: { content: '' }
+        post :create, tweet: { content: '' }
       end
       it "should render #new" do
         expect(response).to render_template 'tweets/new'
@@ -38,17 +38,17 @@ RSpec.describe TweetsController, type: :controller do
     context "if valid" do
       it "should create new tweet" do
         expect{
-          post :create, tweets: { content: 'New content' }
+          post :create, tweet: { content: 'New content' }
         }.to change(Tweet, :count).by 1
       end
 
       it "should have correct redirect" do
-        post :create, tweets: { content: 'New content' }
+        post :create, tweet: { content: 'New content' }
         expect(response).to redirect_to root_path
       end
 
       it "should have correct flash" do
-        post :create, tweets: { content: 'New content' }
+        post :create, tweet: { content: 'New content' }
         expect(flash[:notice]).to eql 'Tweet submitted'
       end
     end