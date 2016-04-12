586eb4f3bf55eb574125a0d74eca49b4b11986b4
Hui Sun <huisun88@gmail.com>
2016-04-12 08:49:12 +0800

    [add] convert currency and post to Twitter

--- app/controllers/robots_controller.rb
+++ app/controllers/robots_controller.rb
@@ -1,4 +1,16 @@
 class RobotsController < ApplicationController
   def index
+    @currencies = Currency.all
+    @tweet_form = TweetForm.new
+  end
+
+  def create
+    @tweet_form = TweetForm.new(tweet_params).create
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet_form).permit(:currency_id)
   end
 end
+
