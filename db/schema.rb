aa5457a8ffaebe5ce49ceb5766021772fd548ae4
Vigneshwaran R <vigneshwaran2007@gmail.com>
2016-04-25 00:34:57 +0530

    schema change

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160424093249) do
+ActiveRecord::Schema.define(version: 20160424185543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 20160424093249) do
     t.integer  "status"
     t.integer  "totalans"
     t.integer  "wrongans"
-    t.string   "addinfo"
+    t.text     "addinfo"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
