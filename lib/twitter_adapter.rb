2c3bfee9b83debc946c5482ced7b860415bb0fd8
Artyom Tsaplin <artyom@loopme.com>
2017-07-26 22:13:40 +0300

    Ugly streaming client added.

--- lib/twitter_adapter.rb
+++ lib/twitter_adapter.rb
@@ -12,4 +12,30 @@ class TwitterAdapter
   def tweet(message)
     client.update(message)
   end
+
+  def self.streaming_client
+    Twitter::Streaming::Client.new do |config|
+      config.consumer_key        = "qQ9i0OO0DTDlp90vU7amaF5lt"
+      config.consumer_secret     = "vlMhOLkjOSw5kOVBwcQKZq4cicC8uqbXSFcYycw7fmjERabbsi"
+      config.access_token        = "505630624-zFJ3erkF4gaeS4o6G9GT9vYHl8WiWG9iIDUVj60s"
+      config.access_token_secret = "Tw79CRa97qWpFYLBCs6dfIlh1bhwIOWNFGyWV0yi7YRMC"
+    end
+  end
+
+  def self.start_streaming
+    streaming_client.user do |object|
+      puts ?**10
+      puts object.class.name
+      case object
+      when Twitter::Tweet
+        puts object.text
+        respond_to = object.in_reply_to_user_id
+        TwitterAdapter.new.tweet('Get ya!', in_reply_to_user_id: respond_to)
+      when Twitter::DirectMessage
+        puts object.text
+      when Twitter::Streaming::Event
+        warn object.name
+      end
+    end
+  end
 end
