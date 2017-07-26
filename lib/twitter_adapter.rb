2477a8d843e32d70e7d4bf118e942d2761552236
Artyom Tsaplin <artyom@loopme.com>
2017-07-26 20:52:42 +0300

    Use access token to be able to tweet.

--- lib/twitter_adapter.rb
+++ lib/twitter_adapter.rb
@@ -4,6 +4,8 @@ class TwitterAdapter
     @client = Twitter::REST::Client.new do |config|
       config.consumer_key        = "qQ9i0OO0DTDlp90vU7amaF5lt"
       config.consumer_secret     = "vlMhOLkjOSw5kOVBwcQKZq4cicC8uqbXSFcYycw7fmjERabbsi"
+      config.access_token        = "505630624-zFJ3erkF4gaeS4o6G9GT9vYHl8WiWG9iIDUVj60s"
+      config.access_token_secret = "Tw79CRa97qWpFYLBCs6dfIlh1bhwIOWNFGyWV0yi7YRMC"
     end
   end
 
