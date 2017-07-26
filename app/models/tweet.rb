0c5017b061d4d7168bb0fc2a739b51722a6e4778
Artyom Tsaplin <artyom@loopme.com>
2017-07-26 20:55:32 +0300

    Simple test tweets page introduced.

---
+++ app/models/tweet.rb
@@ -0,0 +1,19 @@
+class Tweet
+  include ActiveModel::Validations
+  include ActiveModel::Conversion
+  extend ActiveModel::Naming
+
+  attr_accessor :text
+
+  validates :text, :presence => true
+
+  def initialize(attributes = {})
+    attributes.each do |name, value|
+      send("#{name}=", value)
+    end
+  end
+
+  def persisted?
+    false
+  end
+end
