963f72633277d6741a702bed0d5709427491f935
Artyom Tsaplin <artyom@loopme.com>
2017-07-26 21:19:34 +0300

    Random twitter introduced.

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -5,6 +5,19 @@ class Tweet
 
   attr_accessor :text
 
+  PREDEFINED_TWEETS = [
+    "Find anagrams in others tweets",
+    "Look for accidental haiku in tweets and reformat then retweet",
+    "Looks for song lyrics in tweets and posts song names",
+    "Grabs tweets, puts it through a text-to-speech api and posts a link to the audio",
+    "Two bots playing chess together.",
+    "Random words from a dictionary",
+    "@reply a sample image of a colour if you tweet a hexcode at the bot.",
+    "Snowball poems ",
+    "Sports results",
+    "Battleships"
+  ]
+
   validates :text, :presence => true
 
   def initialize(attributes = {})
